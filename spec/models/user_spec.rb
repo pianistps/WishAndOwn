fa9ab9fcd0943772ebc981d07478f5d6e8f5b085
github username pianistps <github email address pianistps@yahoo.com>
2017-06-08 18:52:39 -0500

    setup testing in user spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "has a valid user factory"
+
+  it "is valid with a name, email, and password"
+
+  it "is invalid without an email"
+
+  it "is invalid without a valid email"
+
+  it "is invalid without a password"
+
+  it "is only valid when the password length is a minimum of 8 characters"
+
+  it "encrypts the password upon saving"
 end
