413be4efbed95a3fe7a8e8cf5eb7e1a64f5661ac
github username pianistps <github email address pianistps@yahoo.com>
2017-06-09 17:30:27 -0500

    fixed first test

--- spec/features/user_features_spec.rb
+++ spec/features/user_features_spec.rb
@@ -1,14 +1,17 @@
 require_relative "../rails_helper.rb"
 
 describe "User Signup", type: :feature do
-  before do
-    @user1 = create(:user)
-  end
 
   it  "successfully signs up" do
-    visit_signup
+    @user = build(:user)
+    visit '/'
+    click_link('Sign Up')
     expect(current_path).to eq('/users/new')
-    user_signup
+    fill_in("user[name]", :with => @user1.name)
+    fill_in("user[email]", :with => @user1.email)
+    fill_in("user[password]", :with => @user1.password)
+    click_button('Create User')
+
     expect(current_path).to eq('/users/1')
     expect(page).to have_content("Welcome #{@user1.name}")
   end
