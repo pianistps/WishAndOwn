08a6d779d2a414cdf4c7f4e241625867ada34f4d
github username pianistps <github email address pianistps@yahoo.com>
2017-06-08 19:57:26 -0500

    added descriptions to tests and future tests for user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,42 +1,61 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it "has a valid user factory with a name, email, and password" do
-    expect(build(:user)).to be_valid
+  describe "Test Factory" do
+
+    it "has a valid user factory with a name, email, and password" do
+      expect(build(:user)).to be_valid
+    end
+
   end
 
-  it "is invalid without a name" do
-    expect(build(:user, name: nil)).not_to be_valid
+  describe "Validations" do
+
+    it "is invalid without a name" do
+      expect(build(:user, name: nil)).not_to be_valid
+    end
+
+    it "is invalid without an email" do
+      expect(build(:user, email: nil)).not_to be_valid
+    end
+
+    it "requires a unique email" do
+      create(:user, email: "user@email.com")
+      user = build(:user, email: "user@email.com")
+      user.valid?
+      expect(user.errors.full_messages).to include("Email has already been taken")
+    end
+
+    it "is invalid without a valid email" do
+      expect(build(:user, email: "user.gmail.com")).not_to be_valid
+    end
+
+    it "is invalid without a password" do
+      expect(build(:user, password: nil)).not_to be_valid
+    end
+
+    it "is only valid when the password length is a minimum of 8 characters" do
+      expect(build(:user, password: "123")).not_to be_valid
+      expect(build(:user, password: "12345678")).to be_valid
+    end
+
+    it "encrypts the password upon saving" do
+      user = build(:user)
+      user.save
+      expect(user.password_digest).to be_truthy
+      expect(user.password_digest).not_to eq(user.password)
+    end
   end
 
-  it "is invalid without an email" do
-    expect(build(:user, email: nil)).not_to be_valid
+  describe "relationships" do
+
   end
 
-  it "requires a unique email" do
-    create(:user, email: "user@email.com")
-    user = build(:user, email: "user@email.com")
-    user.valid?
-    expect(user.errors.full_messages).to include("Email has already been taken")
+  describe "class methods" do
+
   end
 
-  it "is invalid without a valid email" do
-    expect(build(:user, email: "user.gmail.com")).not_to be_valid
-  end
+  describe "instance methods" do
 
-  it "is invalid without a password" do
-    expect(build(:user, password: nil)).not_to be_valid
-  end
-
-  it "is only valid when the password length is a minimum of 8 characters" do
-    expect(build(:user, password: "123")).not_to be_valid
-    expect(build(:user, password: "12345678")).to be_valid
-  end
-
-  it "encrypts the password upon saving" do
-    user = build(:user)
-    user.save
-    expect(user.password_digest).to be_truthy
-    expect(user.password_digest).not_to eq(user.password)
   end
 end
