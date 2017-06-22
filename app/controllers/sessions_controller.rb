4299940d802a4e8b7ca29e14d3caf3eaf9f74e5e
github username pianistps <pianistps@yahoo.com>
2017-06-22 15:13:15 -0500

    trying to add error messages for logging in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,8 +5,13 @@ class SessionsController < ApplicationController\
 
   def create
     @user = User.find_by(email: params[:user][:email])
-    session[:user_id] = @user.id
-    redirect_to user_path(@user)
+    if @user.authenticate(params[:user][:password])
+       session[:user_id] = @user.id
+       redirect_to user_path(@user)
+     else
+       @user.authenticate(params[:user][:password])
+       render new_session_path
+     end
   end
 
   def destroy
