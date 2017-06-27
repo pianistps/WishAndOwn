d530decbdf7d31cce5c956134673ed4d3e45cd68
github username pianistps <pianistps@yahoo.com>
2017-06-27 14:45:14 -0500

    added link to login with facebook and route for facebook login

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   resources :users
   root 'users#index'
   resources :sessions, only: [:new, :create, :destroy]
+  get 'auth/:provider/callback' => 'sessions#create_facebook'
 end
