66d2b1206f390e2e4f6a43126cc00953f1faf3e0
github username pianistps <pianistps@yahoo.com>
2017-06-27 14:17:17 -0500

    migrated database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170608155259) do
+ActiveRecord::Schema.define(version: 20170627184557) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
@@ -18,6 +18,8 @@ ActiveRecord::Schema.define(version: 20170608155259) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "provider"
+    t.string   "uid"
   end
 
 end
