85256cb668514a96ec02c7314a6cf49a4d103f11
github username pianistps <pianistps@yahoo.com>
2017-07-06 12:36:14 -0500

    added a partial for inventory form, added create and new actions in controller for inventory

--- app/controllers/inventories_controller.rb
+++ app/controllers/inventories_controller.rb
@@ -1,13 +1,25 @@
 class InventoriesController < ApplicationController
 
   def index
-    @inventories = Inventory.all
   end
 
   def new
+    @inventory = Inventory.new
   end
 
   def create
+    @inventory = Inventory.new(inventory_params)
+    if @inventory.save
+      redirect_to inventory_path(@inventory)
+    else
+      render new_inventory_path
+    end
+  end
+
+  private
+
+  def inventory_params
+    params.require(:inventory).permit(:name)
   end
 
 end
