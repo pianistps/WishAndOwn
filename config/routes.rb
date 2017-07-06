ae806350fa846e35d9f701ad806cb604bd53e826
github username pianistps <pianistps@yahoo.com>
2017-07-06 12:34:08 -0500

    changed the route to not be nested

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,9 @@ Rails.application.routes.draw do
   resources :items
   resources :wishlists
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :users do
-    resources :inventories
-  end
-  
+  resources :users
+  resources :inventories
+
   root 'users#index'
   resources :sessions, only: [:new, :create, :destroy]
   get 'auth/facebook/callback' => 'sessions#create_facebook'
