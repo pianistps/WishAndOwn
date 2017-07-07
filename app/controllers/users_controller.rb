6d9609e8844734f6640d976cb8d14cecfcc2f97f
github username pianistps <pianistps@yahoo.com>
2017-07-06 23:08:41 -0500

    added :user_id hidden field and space for users controllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :logged_in?, only: [:show]
+  
   def index
   end
 
@@ -18,7 +19,7 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
+    @user = current_user
   end
 
 
