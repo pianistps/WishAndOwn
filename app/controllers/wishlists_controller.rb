5b6fbb9e116d9b6d45ca8acf00eba71315fea35f
github username pianistps <pianistps@yahoo.com>
2017-07-13 14:51:12 -0500

    copied over wishlist controller actions

--- app/controllers/wishlists_controller.rb
+++ app/controllers/wishlists_controller.rb
@@ -1,2 +1,44 @@
 class WishlistsController < ApplicationController
+  before_action :set_wishlist, only: [:show, :edit, :update]
+  before_action :logged_in?, :current_user
+
+  def index
+  end
+
+  def new
+    @wishlist = Wishlist.new
+  end
+
+  def create
+    @wishlist = Wishlist.new(wishlist_params)
+    if @wishlist.save
+      redirect_to wishlist_path(@wishlist)
+    else
+      render new_wishlist_path
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @wishlist.update(wishlist_params)
+      redirect_to wishlist_path(@wishlist)
+    else
+      render edit_wishlist_path(@wishlist)
+    end
+  end
+
+  private
+
+  def wishlist_params
+    params.require(:wishlist).permit(:name, :description, :user_id)
+  end
+
+  def set_wishlist
+    @wishlist = Wishlist.find(params[:id])
+  end
 end
