33c358a7c8f224913376852e58489f047ef816e0
github username pianistps <pianistps@yahoo.com>
2017-07-18 15:42:23 -0500

    added nested resources for items within wishlist and inventory

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
-  resources :items
-  resources :wishlists
+
+  resources :wishlists do
+    resources :items
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users
 
   get '/inventories', to: 'inventories#index', as: :my_inventories
-  resources :inventories, :except => :index
+  resources :inventories, :except => :index do
+    resources :items
+  end
 
   root 'users#index'
   resources :sessions, only: [:new, :create, :destroy]
