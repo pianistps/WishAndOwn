3b96f133deeff1287f2263a51459201879423d85
github username pianistps <pianistps@yahoo.com>
2017-07-18 15:39:43 -0500

    added validations for presence or uniqueness

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,7 @@
 class Item < ApplicationRecord
   has_many :inventory_items
   has_many :wishlist_items
+
+  validates :name, uniqueness: :true
+  validates :name, presence: :true
 end
--- app/models/wishlist.rb
+++ app/models/wishlist.rb
@@ -3,6 +3,7 @@ class Wishlist < ApplicationRecord
   has_many :wishlist_items
   has_many :items, through: :wishlist_items
   validates :name, presence: :true
+  validates :name, uniqueness: :true
 
   def self.user_wishlists(user)
     where(user_id: user.id)
