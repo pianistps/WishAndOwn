a9daa6cebf6ebd11395545ab1c80cafcfcf29d0e
github username pianistps <pianistps@yahoo.com>
2017-07-18 17:05:57 -0500

    added method to set iventory or item for form and started form

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,19 @@
 class ItemsController < ApplicationController
+  before_action :logged_in?, :current_user
+  before_action :inventory_or_wishlist, only: [:new]
 
-  def new 
+  def new
   end
 
+  private
+
+  def inventory_or_wishlist
+    request = self.request.env["REQUEST_PATH"].split("/")
+    if /wishlists/.match(request[1])
+      @parent = Wishlist.find(params[:wishlist_id])
+    elsif /inventories/.match(request[1])
+      @parent = Inventory.find(params[:inventory_id])
+    end
+    @parent
+  end
 end
