34eb5cd51c59856c5321f52dafe9630bffa2fe81
github username pianistps <pianistps@yahoo.com>
2017-07-19 14:42:01 -0500

    added the writer for the items_attributes

--- app/models/inventory.rb
+++ app/models/inventory.rb
@@ -11,8 +11,15 @@ class Inventory < ApplicationRecord
   def items_attributes=(item_hashes)
     item_hashes.each do |i, item_attributes|
       if item_attributes[:name].present?
-        binding.pry
-        item = Item.find_or_create_by(:name, item_attributes[:name])
+        item = Item.find_or_create_by(:name => item_attributes[:name])
+        if item_attributes[:description].present?
+          item[:description] = item_attributes[:description]
+          item.save
+        end
+        if item_attributes[:url].present?
+          item[:url] = item_attributes[:url]
+          item.save
+        end
         if !self.items.include?(item)
           self.inventory_items.build(item: item)
         end
