9becfb514de44d0ab5597cd8ab8cbe3209f46e67
github username pianistps <pianistps@yahoo.com>
2017-07-20 12:16:59 -0500

    added items attributes writer for wishlist

--- app/models/wishlist.rb
+++ app/models/wishlist.rb
@@ -8,4 +8,23 @@ class Wishlist < ApplicationRecord
   def self.user_wishlists(user)
     where(user_id: user.id)
   end
+
+  def items_attributes=(item_hashes)
+    item_hashes.each do |i, item_attributes|
+      if item_attributes[:name].present?
+        item = Item.find_or_create_by(:name => item_attributes[:name])
+        if item_attributes[:description].present?
+          item[:description] = item_attributes[:description]
+          item.save
+        end
+        if item_attributes[:url].present?
+          item[:url] = item_attributes[:url]
+          item.save
+        end
+        if !self.items.include?(item)
+          self.wishlist_items.build(item: item)
+        end
+      end
+    end
+  end
 end
