5d4d2a83a2ef123d7c07b8736f2ec7c40ba73de8
github username pianistps <pianistps@yahoo.com>
2017-07-24 18:22:46 -0500

    added destroy action for inventory and set inventory for destroy

--- app/controllers/inventories_controller.rb
+++ app/controllers/inventories_controller.rb
@@ -1,5 +1,5 @@
 class InventoriesController < ApplicationController
-  before_action :set_inventory, only: [:show, :edit, :update]
+  before_action :set_inventory, only: [:show, :edit, :update, :destroy]
   before_action :logged_in?, :current_user
   before_action :clean_join_tables, only: [:update]
 
@@ -35,6 +35,11 @@ class InventoriesController < ApplicationController
     else
       render edit_inventory_path(@inventory)
     end
+  end
+
+  def destroy
+    @inventory.destroy
+    redirect_to my_inventories_path
   end
 
   private
