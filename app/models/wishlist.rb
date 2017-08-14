f9b96440acb939ba7d741f9feecc016f6dd141a6
github username pianistps <pianistps@yahoo.com>
2017-08-14 11:54:03 -0500

    added dependent destroy and added checkbox for adding wishlist items to inventory

--- app/models/wishlist.rb
+++ app/models/wishlist.rb
@@ -1,6 +1,6 @@
 class Wishlist < ApplicationRecord
   belongs_to :user
-  has_many :wishlist_items
+  has_many :wishlist_items, dependent: :destroy
   has_many :items, through: :wishlist_items, dependent: :destroy
   validates :name, presence: :true
   validates :name, uniqueness: :true
